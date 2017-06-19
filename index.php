f1401c228609aa3ae37ab7277072ca36d0cd43cf
manuelgvmx <mauro@manuelgv.mx>
2017-06-19 14:23:13 -0500

    Primeros Cambios

--- index.php
+++ index.php
@@ -3,10 +3,19 @@
 <head>
 	<title>SysWebGastos</title>
 	<meta charset="utf-8">
+	<script type="text/javascript" src="js/index.js"></script>
 </head>
 <body>
-<?php
-	print("Hola Mundo");
-?>
+<header>
+	<h1><em>Sys</em>tema <em>Web</em> de control de <em>Gastos</em></h1>
+</header>
+<div>
+	
+	
+
+</div>
+<footer>
+	<div>2017 &copy; Derechos Reservados | <a href="https://twitter.com/manuelgvmx">@ManuelGVMX</a></div>
+</footer>
 </body>
 </html>
